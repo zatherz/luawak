9e916c9b10aa4e26fce58318b5d13b6a1bfb9304
Zatherz <zatherz@linux.pl>
2020-05-16 21:18:44 +0200

    Full modify/write support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LuaWAK
 
-LuaJIT library using FFI to read .wak files (like the Noita `data.wak`).
+LuaWAK is a single-file LuaJIT library that makes use of FFI to read, alter and write files in the Noita WizardPak (.wak) format, such as the game's assets archive `data.wak`.
 
 # Basic Usage
 
@@ -15,7 +15,7 @@ local ar = wak.open("data/data.wak") -- open the .wak file
 
 local file = ar:open("data/enemies_gfx/iceskull.png") -- open any file within the data.wak
 
-file:write_to("iceskull.png") -- write the data into a file on-disk
+file:write("iceskull.png") -- write the data into a file on-disk
 ```
 
 ## Reading files
@@ -27,30 +27,145 @@ local materials_file = ar:open("data/materials.xml")
 local materials_xml = materials_file:read() -- read the contents of the file into a string
 
 print(materials_xml)
+
+for file in ar:files() -- iterate all the files in the archive
+    print(file)
+end
 ```
 
+## Modifying archives
+```lua
+local wak = require("wak")
+local ar = wak.open("data/data.wak")
+
+ar:set("data/materials.xml", "<Materials> </Materials>") -- set the contents of the file
+
+ar:add("data/my_file.txt", "Hello!") -- add a new file
+
+local my_file = ar:open("data/my_file.txt") 
+print(my_file:read()) -- added files work like normal
+
+ar:write("data/data.wak.new") -- write the modified archive
+```
+
+## Creating new archives 
+
+```lua
+local wak = require("wak")
+local ar = wak.new()
+
+ar:add("1.txt", "One") -- add a file
+ar:add("2.txt", "Two") -- add another file
+ar:set("3.txt", "Three") -- :set does the same thing
+
+ar:write("my.wak") -- write the new archive to disk
+```
 # API
 
 * global table `wak`
-	* function `open(path) -> wak_archive` - opens a .wak file and returns its representation as a `wak_archive` object
+	* function `open(path : string, lenient : boolean?) : wak_archive`
+
+      opens a .wak file and returns its representation as a `wak_archive` object  
+      if the `lenient` optional argument is passed as `true` the function will return `nil` if the file can't be opened, otherwise an error will be raised
+
+    * function `new() : wak_archive`
+
+      creates a new in-memory wak archive
 
 * type `wak_archive`
-	* field `path` - contains the path that the archive has been loaded from
-	* function `preload_all() -> nil` - preloads metadata about all files
-	* function `open(path) -> wak_file` - opens a file within the archive and returns it
-	* function `extract(path, out_path) -> nil` - extracts a file from the archive onto an on-disk file, will do nothing if `path` doesn't exist in the archive
-	* function `dispose() -> nil` - closes the underlying file stream and disposes of the archive's resources, rendering it unusable
+	* field `path`
 
-  note: `dispose()` is called automatically when the archive object is garbage collected; calling it more than one time is perfectly valid and has no effect  
+      contains the path that the archive has been loaded from  
+      initialized with `"(memory)"` for archives created with `wak.new()`
+	* function `preload_all()`
+
+      preloads metadata about all files  
+      luawak conserves memory and maintains speed by avoiding reading the entire file - instead, chunks are loaded as they are needed, e.g. to grab a particular file; this function forces the entire file to be loaded (but note that the file stream will still be owned by the object)
+
+	* function `open(path : string) : wak_file` 
+
+      opens a file within the archive and returns it
+
+	* function `extract(path : string, out_path : string)`
+
+      extracts a file from the archive into an on-disk file  
+      will do nothing if `path` doesn't exist in the archive
+
+    * function `add(path : string, content : string) : wak_file`
+
+      adds a file named `path` with `content` as the data to the archive  
+      returns the `wak_file` object that was added  
+      will raise an error if the file exists
+      will cause the entire archive to be loaded into memory
+
+    * function `set(path : string, content : string) : wak_file`
+
+      adds or changes a file named `path` in the archive, setting its content to `content`  
+      returns the `wak_file` object that was added  
+      will work for both existing files and new files  
+      will not load any more data from the archive than is already in memory
+
+    * function `import(path : string, imp_path : string) : wak_file`
+
+      adds or changes a file named `path` in the archive, setting its content to the data in a file specified by `imp_path`  
+      same behavior as `:set`
+
+    * function `remove(path : string)`
+
+      removes the file at `path` from the archive  
+      will do nothing if the file doesn't exist
+    
+    * function `count_files() : number`
+
+      returns a count of all files in the archive, including files added by `:add`/`:set`/`:import`  
+      will cause the entire archive to be loaded into memory
+
+    * function `files() : iterator<wak_file>`
+
+      the `:files` function acts as an iterator, providing a `wak_file` object each loop  
+      it's used similar to `ipairs` and `pairs`: `for file in archive:files()`
+
+    * function `force_rebuild_on_write()`
+
+      by default, if no changes are made to an archive, `:write` is essentially just a copy of the file  
+      if changes *are* made, the function automatically switches to rebuilding the entire archive from scratch  
+      if this function is called once before any calls to `:write`, luawak will drop the above behavior and always choose to rebuild the archive
+
+    * function `write(out_path : string)`
+
+      writes the whole archive to a file on disk  
+      if the archive has been modified or `:force_rebuild_on_write` has been called before, the archive will be rebuilt from scratch at the provided file path  
+      this function will reject writing to an `out_path` that is the same as the path the archive had been loaded from due to luawak's streaming behavior - see `:write_and_dispose` below
+
+    * function `write_and_dispose(out_path : string)`
+
+      same behavior as `:write`  
+      however, after this function quits, the object is disposed and unusable (see `:dispose`)  
+      this method **can** write to the same `out_path` as the path that the archive was initially loaded from
+
+	* function `dispose()`
+
+      closes the underlying file stream and disposes of the archive's resources, rendering it unusable  
+      note: `dispose()` is called automatically when the archive object is garbage collected; calling it more than one time is perfectly valid and has no effect  
+      calling any functions on a disposed object will raise errors
 
 * type `wak_file`
-	* field `length` - contains the length of the file in bytes
-	* field `path` - contains the path to the file within the .wak archive
-	* function `read() -> string` - returns the data of the file as a string
-	* function `write_to(out_path) -> nil` - writes the wak file into an on-disk file at the path specified by `out_path`
+	* field `length`
 
-  note: when the original archive is disposed, all files become unusable; do take care in ensuring the original `wak_archive` object is preserved somewhere
+      length of the file in bytes
+
+	* field `path`
+
+      path to the file within the .wak archive
+
+	* function `read() : string`
+
+      returns the data of the file as a string
+
+	* function `write(out_path) nil`
+
+      writes the wak file into an on-disk file at the path specified by `out_path`
 
 # License
 
-MIT. See `LICENSE` in this repository.
+LuaWAK is licensed under the MIT license. See `LICENSE` in this repository.
--- wak.lua
+++ wak.lua
@@ -5,11 +5,30 @@ ffi.cdef[[
 void* fopen(const char* filename, const char* mode);
 size_t fwrite(void* ptr, size_t size, size_t count, void* stream);
 size_t fread(void* ptr, size_t size, size_t count, void* stream);
+long int ftell(void* stream);
 int fseek(void* stream, long int offset, int origin);
 void fclose(void* stream);
 int ferror(void* stream);
 int feof(void* stream);
+void* memcpy(void* dest, void* src, size_t num);
 ]]
+
+local function check_arg(name, expected_type, val)
+    local t = type(val)
+    if t ~= expected_type then
+        if expected_type:sub(-1, -1) == "?" then
+            return
+        end
+        error(
+            "invalid type for argument "
+            .. tostring(name)
+            .. ": expected "
+            .. tostring(expected_type)
+            .. ", got "
+            .. t
+        )
+    end
+end
 
 local function safe_fread(ptr, size, count, stream)
     local count_read = ffi.C.fread(ptr, size, count, stream);
@@ -17,15 +36,28 @@ local function safe_fread(ptr, size, count, stream)
         -- for our purposes, both unexpected eof and actual error are errors
         if ffi.C.ferror(stream) ~= 0 or ffi.C.feof(stream) ~= 0 then
             error(
-            "archive is corrupted - unexpected end of stream (tried reading "
-            .. tostring(count)
-            .. " byte(s), but only received "
-            .. tostring(count_read)
-            .. ")"
+                "archive is corrupted - unexpected end of stream (tried reading "
+                .. tostring(count)
+                .. " byte(s), but only received "
+                .. tostring(count_read)
+                .. ")"
             )
         end
     end
     return count_read
+end
+
+local function safe_fopen(path, mode, lenient)
+    local fd = ffi.C.fopen(path, mode)
+    if lenient then return fd end
+    if fd == nil then
+        error(
+            "file doesn't exist: '"
+            .. tostring(path)
+            .. "'"
+        )
+    end
+    return fd
 end
 
 local ALLOC_SIZE_LIMIT = 128 * 1024 * 1024 -- 128MB ought to be enough for everybody
@@ -82,8 +114,26 @@ local function read_toc(wak, predicate)
         wak._pos = wak._pos + path_length
         local path = ffi.string(path_bytes, path_length)
 
-        local file = setmetatable({ _wak = wak, _offset = offs, length = length, path = path }, WAK_FILE_MT)
-        wak._files[path] = file
+        local file = setmetatable({
+            _wak = wak,
+            _offset = offs,
+            length = length,
+            path = path,
+
+            _content = nil
+            -- if this is set, it should contain
+            -- a lua string, which will override
+            -- the behavior of functions such as
+            -- read()
+        }, WAK_FILE_MT)
+        local existing_file = wak._files[path]
+        if not existing_file then
+            -- we don't care if it's a _removed entry
+            -- here, because we also want to avoid
+            -- overwriting those (otherwise sometimes
+            -- :remove would just not work)
+            wak._files[path] = file
+        end
 
         wak._read_files = wak._read_files + 1
 
@@ -95,12 +145,15 @@ local function read_toc(wak, predicate)
     return contents
 end
 
-function wak.open(path)
+function wak.open(path, lenient)
+    check_arg("path", "string", path)
+    check_arg("lenient", "boolean?", lenient)
+
     if path:sub(-4, -1) ~= ".wak" then
         error("archive path must end with .wak extension: '" .. path .. "'")
     end
 
-    local wak_f = ffi.C.fopen(path, "rb")
+    local wak_f = safe_fopen(path, "rb", lenient)
     if wak_f == nil then return nil end
 
     ffi.C.fseek(wak_f, INT_SIZE, 0)
@@ -119,7 +172,8 @@ function wak.open(path)
         _toc_size = toc_size,
         _pos = INT_SIZE * 4,
         _read_files = 0,
-        _files = {}
+        _files = {},
+        _should_rebuild = false
     }, WAK_MT)
 
     local ar_proxy = newproxy(true)
@@ -138,40 +192,315 @@ function wak.open(path)
     return ar_proxy
 end
 
+function wak.new()
+    return setmetatable({
+        path = "(memory)",
+        _stream = nil,
+        _num_files = 0,
+        _toc_size = 0,
+        _pos = 0,
+        _read_files = 0,
+        _files = {},
+        _should_rebuild = true
+    }, WAK_MT)
+end
+
 local function check_disposed(wak)
     if wak._disposed then error("cannot operate on disposed archive") end
 end
 
 function WAK_FUNCS:preload_all()
     check_disposed(self)
+    if not self._stream then
+        return
+    end
     read_toc(self)
 end
 
 function WAK_FUNCS:open(path)
     check_disposed(self)
+    check_arg("path", "string", path)
+    
     local existing_file = self._files[path]
-    if existing_file ~= nil then return existing_file end
-    read_toc(self, function(file) return file.path == path end)
+    if existing_file ~= nil then
+        if existing_file._removed then return nil end
+        return existing_file
+    end
+
+    if self._stream then
+        read_toc(self, function(file) return file.path == path end)
+    end
     return self._files[path]
 end
 
 function WAK_FUNCS:extract(path, out_path)
     check_disposed(self)
+    check_arg("path", "string", path)
+    check_arg("out_path", "string", out_path)
+
+    local existing_file = self._files[path]
+    if existing_file then
+        if existing_file._removed then
+            error("File was removed from the archive: " .. path)
+        end
+        existing_file:write(out_path)
+        return
+    end
+
+    if not self._stream then return end
+
+    local found = false
     read_toc(function(file)
         if file.path == path then
-            ffi.C.fseek(self._wak._stream, offs, 0)
-            check_junk_size(self.length, "extract buffer")
-            local buf = ffi.new("char[?]", self.length)
-            safe_fread(buf, ffi.sizeof("char"), self.length, self._wak._stream)
-
-            local out_f = ffi.C.fopen(out_path, "w")
-            ffi.C.fwrite(buf, ffi.sizeof("char"), self.length, out_f)
-
-            ffi.C.fclose(out_f)
+            found = true
+            file:write(out_path)
 
             return true
         end
     end)
+
+    if not found then
+        error("File doesn't exist in the archive: " .. path)
+    end
+end
+
+function WAK_FUNCS:add(path, content)
+    check_disposed(self)
+    check_arg("path", "string", path)
+    check_arg("content", "string", content)
+
+    self:preload_all()
+    local existing_file = self._files[path]
+    if existing_file and not existing_file._removed then
+        error(
+            "wak file already exists: '"
+            .. path
+            .. "'"
+        )
+    end
+    return self:set(path, content)
+end
+
+function WAK_FUNCS:set(path, content)
+    check_disposed(self)
+    check_arg("path", "string", path)
+    check_arg("content", "string", content)
+
+    local memory_file = setmetatable({
+        _wak = self,
+        path = path,
+        length = #content,
+        _content = content
+        -- _content overrides the default behavior
+        -- of reading the file
+    }, WAK_FILE_MT)
+
+    self._should_rebuild = true
+    -- if a file is added to the wak (or changed),
+    -- writing it is no longer just a simple
+    -- bytewise copy
+
+    self._files[path] = memory_file
+    return memory_file
+end
+
+function WAK_FUNCS:import(path, imp_path)
+    check_disposed(self)
+    check_arg("path", "string", path)
+    check_arg("imp_path", "string", imp_path)
+
+    local imp_f = safe_fopen(imp_path, "rb")
+    ffi.C.fseek(imp_f, 0, 2)
+    local imp_len = ffi.C.ftell(imp_f)
+    ffi.C.fseek(imp_f, 0, 0)
+    local buf = ffi.new("const char[?]", imp_len)
+    safe_fread(ffi.cast("void*", buf), 1, imp_len, imp_f)
+    ffi.C.fclose(imp_f)
+
+    return self:set(path, ffi.string(buf, imp_len))
+end
+
+function WAK_FUNCS:remove(path)
+    check_disposed(self)
+    check_arg("path", "string", path)
+
+    self._files[path] = { _removed = true }
+end
+
+function WAK_FUNCS:count_files()
+    check_disposed(self)
+
+    local count = 0
+    self:preload_all()
+    for k, v in pairs(self._files) do
+        if not v._removed then
+            count = count + 1
+        end
+    end
+    return count
+end
+
+function WAK_FUNCS:files()
+    check_disposed(self)
+
+    self:preload_all()
+    local gen, state, k = pairs(self._files)
+
+    local function files_gen()
+        local v
+        repeat
+            k, v = gen(state, k) 
+        until k == nil or not v._removed
+
+        if k ~= nil then
+            return v
+        end
+    end
+
+    return files_gen
+end
+
+local function wak_file_get_buf(self)
+    local buf
+    if self._content then
+        buf = ffi.cast(
+            "void*",
+            ffi.cast("const char*", self._content)
+        )
+    else
+        ffi.C.fseek(self._wak._stream, self._offset, 0)
+        check_junk_size(self.length, "file " .. self.path)
+        buf = ffi.new("char[?]", self.length)
+        safe_fread(buf, ffi.sizeof("char"), self.length, self._wak._stream)
+    end
+    return buf
+end
+
+local function build_archive(wak)
+    -- we will first get the total size of the buffer
+    -- then iterate again
+    -- it's simpler than dynamically reallocating
+    -- and probably actually faster despite the extra
+    -- loop
+
+    local header_size = 4 * INT_SIZE
+    local buf_size = header_size -- for the header
+    local file_count = 0
+    local toc_size = 0
+    for path, file in pairs(wak._files) do
+        if not file._removed then
+            file_count = file_count + 1
+            local toc_delta = INT_SIZE * 3 + #file.path
+            toc_size = toc_size + toc_delta
+            buf_size = buf_size + toc_delta + file.length
+            -- offset, length, pathlength, path, and 
+            -- finally content (in the block later)
+        end
+    end
+
+    local buf = ffi.new("char[?]", buf_size)
+    
+    local header_unknown_1_ptr = ffi.cast("int*", buf + 0 * INT_SIZE)
+    local num_files_ptr = ffi.cast("int*", buf + 1 * INT_SIZE)
+    local toc_size_ptr = ffi.cast("int*", buf + 2 * INT_SIZE)
+    local header_unknown_2_ptr = ffi.cast("int*", buf + 3 * INT_SIZE)
+
+    header_unknown_1_ptr[0] = 0
+    num_files_ptr[0] = file_count
+    toc_size_ptr[0] = toc_size
+    header_unknown_2_ptr[0] = 0
+
+    local offset = header_size + toc_size
+    local buf_offs = header_size -- skip header
+    for path, file in pairs(wak._files) do 
+        if not file._removed then
+            local offs_ptr = ffi.cast("int*", buf + buf_offs + 0 * INT_SIZE)
+            local length_ptr = ffi.cast("int*", buf + buf_offs + 1 * INT_SIZE)
+            local pathlen_ptr = ffi.cast("int*", buf + buf_offs + 2 * INT_SIZE)
+            local path_ptr = buf + buf_offs + 3 * INT_SIZE
+
+            offs_ptr[0] = offset
+            length_ptr[0] = file.length
+            pathlen_ptr[0] = #file.path
+
+            local path_buf = ffi.cast("const char*", file.path)
+            for i = 0, #file.path - 1 do
+                path_ptr[i] = path_buf[i]
+            end
+
+            offset = offset + file.length
+            buf_offs = buf_offs + 3 * INT_SIZE + #file.path
+        end
+    end
+
+    local i = 0
+    for path, file in pairs(wak._files) do
+        if not file._removed then
+            i = i + 1
+            local file_buf = wak_file_get_buf(file)
+            ffi.C.memcpy(buf + buf_offs, file_buf, file.length)
+            buf_offs = buf_offs + file.length
+        end
+    end
+
+    return { buf = buf, buf_size = buf_size }
+end
+
+local function write_wak(self, out_path, dispose)
+    self:preload_all()
+
+    if not dispose and out_path == self.path then
+        error(
+            "Archive '"
+            .. self.path
+            .. "' may not be written to the same path "
+            .. "without using :write_and_dispose"
+        )
+    end
+
+    local buf = nil
+    local buf_size = 0
+    if self._should_rebuild then
+        local ar = build_archive(self, f)
+        buf = ar.buf
+        buf_size = ar.buf_size
+    else
+        ffi.C.fseek(self._stream, 0, 2)
+        buf_size = ffi.C.ftell(self._stream)
+        check_junk_size(buf_size, "size of archive on disk")
+        ffi.C.fseek(self._stream, 0, 0)
+        buf = ffi.new("char[?]", buf_size)
+
+        safe_fread(buf, 1, buf_size, self._stream)
+        ffi.C.fseek(self._stream, self._pos, 0)
+    end
+
+    if dispose then
+        self:dispose()
+    end
+    local f = safe_fopen(out_path, "wb")
+
+    ffi.C.fwrite(buf, 1, buf_size, f)
+    ffi.C.fclose(f)
+end
+
+function WAK_FUNCS:force_rebuild_on_write()
+    check_disposed(self)
+    self._should_rebuild = true
+end
+
+function WAK_FUNCS:write(out_path)
+    check_disposed(self)
+    check_arg("out_path", "string", out_path)
+
+    write_wak(self, out_path, false)
+end
+
+function WAK_FUNCS:write_and_dispose(out_path)
+    check_disposed(self)
+    check_arg("out_path", "string", out_path)
+
+    write_wak(self, out_path, true)
 end
 
 function WAK_FUNCS:dispose()
@@ -183,21 +512,25 @@ end
 
 function WAK_FILE_FUNCS:read()
     check_disposed(self._wak)
+
+    if self._content then
+        return self._content
+    end
+
     ffi.C.fseek(self._wak._stream, self._offset, 0)
     check_junk_size(self.length, "file " .. self.path)
     local buf = ffi.new("char[?]", self.length)
     safe_fread(buf, ffi.sizeof("char"), self.length, self._wak._stream)
+    ffi.C.fseek(self._wak._stream, self._wak._pos, 0)
     return ffi.string(buf, self.length)
 end
 
-function WAK_FILE_FUNCS:write_to(out_path)
+function WAK_FILE_FUNCS:write(out_path)
     check_disposed(self._wak)
-    ffi.C.fseek(self._wak._stream, self._offset, 0)
-    check_junk_size(self.length, "file " .. self.path)
-    local buf = ffi.new("char[?]", self.length)
-    safe_fread(buf, ffi.sizeof("char"), self.length, self._wak._stream)
 
-    local out_f = ffi.C.fopen(out_path, "w")
+    local buf = wak_file_get_buf(self)
+
+    local out_f = safe_fopen(out_path, "w")
     ffi.C.fwrite(buf, ffi.sizeof("char"), self.length, out_f)
 
     ffi.C.fclose(out_f)
